f8426fbad671d2f1b93ab25ecb9e0a73d7157df5
Aaleia <aaleia.fernando@gmail.com>
2023-02-08 12:40:52 -0500

    Finished 2B

--- funcs.h
+++ funcs.h
@@ -1,3 +1,11 @@
+/*
+Author: Aaleia Fernando
+Course: CSCI-135
+Instructor: Mike Zamansky
+Assignment: Lab 2B
+In funcs.cpp write a void function that will accept two integer parameters and will print out all integers in the range L ≤ i < U separated by spaces. Test this function by calling it from the main function which should be defined in main.cpp. Make sure to include sufficient tests to from main to show that this works.
+*/
+
 #pragma once
 
 void print_interval(int, int);
